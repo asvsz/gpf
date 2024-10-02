6eaf78326a87f06967356eaae21039d3592c1e23
Amanda Souza <amandasilvadsz@gmail.com>
2024-10-02 15:14:06 -0300

    feat: patient registration page

--- app/CadastrarPaciente/page.tsx
+++ app/CadastrarPaciente/page.tsx
@@ -5,16 +5,19 @@ import { useAuth } from '../context/AuthContext';
 import ButtonOne from '../components/ButtonOne';
 import { useRouter } from 'next/navigation';
 
-export default function CadastrarMedico() {
+export default function CadastrarPaciente() {
   const { user } = useAuth();
-
-  const router = useRouter()
+  const router = useRouter();
 
   const [name, setName] = useState('');
   const [surname, setSurname] = useState('');
   const [gender, setGender] = useState('');
-  const [occupation, setOccupation] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+  const [cpf, setCpf] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
@@ -22,39 +25,49 @@ export default function CadastrarMedico() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const newClinician = {
+    const formattedBirthDate = new Date(birthDate).toISOString();
+
+    const newPatient = {
       name,
       surname,
       gender,
-      occupation,
+      birthDate: formattedBirthDate, // Usando a data formatada
+      cpf,
       phoneNumber,
+      address,
+      city,
+      state,
       email,
       password,
     };
 
     try {
-      const response = await api.post(`/${user}s`, newClinician);
-      setMessage('Fisioterapeuta registrado com sucesso!');
-      console.log('Fisioterapeuta registrado com sucesso!');
-      // Clear fields after registration
+      const response = await api.post(`/${user}s`, newPatient);
+      setMessage('Paciente registrado com sucesso!');
+      console.log('Paciente registrado com sucesso!', response.data);
+      // Limpa os campos após o registro
       setName('');
       setSurname('');
       setGender('');
-      setOccupation('');
+      setBirthDate('');
+      setCpf('');
       setPhoneNumber('');
+      setAddress('');
+      setCity('');
+      setState('');
       setEmail('');
       setPassword('');
 
-      router.push('/Login')
+      router.push('/Login');
     } catch (error) {
-      setMessage('Erro ao registrar o fisioterapeuta. Por favor, tente novamente.');
+      setMessage('Erro ao registrar o paciente. Por favor, tente novamente.');
       console.error(error);
     }
   };
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Registrar Fisioterapeuta</h1>
+      <h1 className="text-2xl font-bold mb-4">Registrar Paciente</h1>
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
           <label className="block text-gray-700">Nome</label>
@@ -94,11 +107,21 @@ export default function CadastrarMedico() {
           </select>
         </div>
         <div>
-          <label className="block text-gray-700">Especialização</label>
+          <label className="block text-gray-700">Data de Nascimento</label>
+          <input
+            type="date"
+            value={birthDate}
+            onChange={(e) => setBirthDate(e.target.value)}
+            className="w-full px-3 py-2 border rounded-md"
+            required
+          />
+        </div>
+        <div>
+          <label className="block text-gray-700">CPF</label>
           <input
             type="text"
-            value={occupation}
-            onChange={(e) => setOccupation(e.target.value)}
+            value={cpf}
+            onChange={(e) => setCpf(e.target.value)}
             className="w-full px-3 py-2 border rounded-md"
             required
           />
@@ -113,6 +136,37 @@ export default function CadastrarMedico() {
             required
           />
         </div>
+        <div>
+          <label className="block text-gray-700">Endereço</label>
+          <input
+            type="text"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            className="w-full px-3 py-2 border rounded-md"
+            required
+          />
+        </div>
+        <div>
+          <label className="block text-gray-700">Cidade</label>
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            className="w-full px-3 py-2 border rounded-md"
+            required
+          />
+        </div>
+        <div>
+          <label className="block text-gray-700">Estado</label>
+          <input
+            type="text"
+            value={state}
+            onChange={(e) => setState(e.target.value)}
+            className="w-full px-3 py-2 border rounded-md"
+            required
+          />
+        </div>
+
         <div>
           <label className="block text-gray-700">E-mail</label>
           <input

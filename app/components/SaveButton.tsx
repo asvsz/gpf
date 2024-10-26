4021fa0fb96253b4ccab99bcb7cd3be69f60333e
Amanda Souza <amandasilvadsz@gmail.com>
2024-10-26 15:04:54 -0300

    style: changing color from blue to green

--- app/components/SaveButton.tsx
+++ app/components/SaveButton.tsx
@@ -9,7 +9,7 @@ export default function SaveButton ({ onClick, label = "Salvar" }: SaveButtonPro
   return (
     <button
       onClick={onClick}
-      className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition"
+      className="bg-green-800 text-white px-4 py-2 rounded-md hover:bg-green-600 transition"
     >
       {label}
     </button>

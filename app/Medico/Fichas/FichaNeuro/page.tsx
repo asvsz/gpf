581aba1eeeee7872630f9a1b0409e1dc78ced067
Amanda Souza <amandasilvadsz@gmail.com>
2024-11-03 18:26:40 -0300

    style: improved appearance of the FichaNeuro page form

--- app/Medico/Fichas/FichaNeuro/page.tsx
+++ app/Medico/Fichas/FichaNeuro/page.tsx
@@ -4,6 +4,8 @@ import api from "@/app/services/api";
 import PrivateRoute from '@/app/components/PrivateRoute';
 import NavbarDoctor from "@/app/components/NavbarDoctor";
 import Footer from "@/app/components/Footer";
+import { useRouter } from 'next/navigation';
+import ButtonOne from '@/app/components/ButtonOne';
 
 interface NeurofunctionalRecordProps {
     medicalDiagnosis: string;
@@ -79,6 +81,8 @@ export default function ViewNeurofunctionalRecord() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
+    const router = useRouter()
+
     // Função para buscar os dados do registro neurofuncional
     const fetchRecordData = async () => {
         const recordId = localStorage.getItem('currentRecordId');
@@ -122,75 +126,81 @@ export default function ViewNeurofunctionalRecord() {
     return (
         <PrivateRoute requiredUserType='clinician'>
             <div className="flex flex-col min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <NavbarDoctor/>
-                <h2 className=" text-3xl font-bold">Visualizar Registro Neurofuncional</h2>
+                <NavbarDoctor />
+                <h2 className="font-bold text-4xl text-gray-700 pt-8">Visualizar Registro Neurofuncional</h2>
 
                 {error && <div className="text-red-500 mb-4">{error}</div>}
 
-                {loading  ? (
-                <div className="flex justify-center items-center h-64">
-                    <span>Carregando...</span>
-                </div>
-                ): record ? (
-                    <div className="flex flex-col mb-10">
-                        <div>
-                            <h3 className="font-semibold">Diagnóstico Médico:</h3>
+                {loading ? (
+                    <div className="flex justify-center text-lg text-gray-500 items-center h-64">
+                        <span>Carregando...</span>
+                    </div>
+                ) : record ? (
+                    <div className="grid gap-8 pb-8">
+                        <div className="bg-gray-100 shadow-lg rounded-lg p-6">
+                            <h3 className="text-2xl block text-gray-700 font-medium pb-6">Diagnóstico Médico:</h3>
                             <p>{record.medicalDiagnosis}</p>
                         </div>
 
-                        <div>
-                            <h3 className="font-semibold">Anamnese:</h3>
+                        <div className="bg-gray-100 shadow-lg rounded-lg p-6">
+                            <h3 className="text-2xl block text-gray-700 font-medium pb-6">Anamnese:</h3>
                             <p>{record.anamnesis}</p>
                         </div>
 
-                        <div>
-                            <h3 className="font-semibold">Exame Físico:</h3>
+                        <div className="bg-gray-100 shadow-lg rounded-lg p-6">
+                            <h3 className="text-2xl block text-gray-700 font-medium pb-6">Exame Físico:</h3>
                             <p>{record.physicalExamination}</p>
                         </div>
 
-                        <div>
-                            <h3 className="font-semibold">Triagem:</h3>
+                            <div className="flex flex-col gap-4 bg-gray-100 shadow-lg rounded-lg p-6">
+                                <h3 className="text-2xl block text-gray-700 font-medium pb-6">Triagem:</h3>
                             <p>{record.triage}</p>
                         </div>
 
-                        <div>
-                            <h3 className="font-semibold">Hábitos de Vida:</h3>
-                            <p>Consome Álcool: {record.lifestyleHabits.alcoholConsumption ? 'Sim' : 'Não'}</p>
-                            <p>Fumante: {record.lifestyleHabits.smoker ? 'Sim' : 'Não'}</p>
+                            <div className="flex flex-col gap-4 bg-gray-100 shadow-lg rounded-lg p-6">
+                                <h3 className="text-2xl block text-gray-700 font-medium pb-6">Hábitos de Vida:</h3>
+                                <p className='flex gap-2'><h3 className='font-semibold'>Consome Álcool:</h3> {record.lifestyleHabits.alcoholConsumption ? 'Sim' : 'Não'}</p>
+                                <p className='flex gap-2'><h3 className='font-semibold'>Fumante:</h3> {record.lifestyleHabits.smoker ? 'Sim' : 'Não'}</p>
                         </div>
 
-                        <div>
-                            <h3 className="font-semibold">Sinais Vitais:</h3>
-                            <p>Pressão Arterial: {record.vitalSigns.bloodPressure}</p>
-                            <p>Frequência Cardíaca: {record.vitalSigns.heartRate}</p>
+                        <div className="flex flex-col gap-4 bg-gray-100 shadow-lg rounded-lg p-6">
+                                <h3 className="text-2xl block text-gray-700 font-medium pb-6">Sinais Vitais:</h3>
+                                <p className='flex gap-2'><h3 className='font-semibold'>Pressão Arterial: </h3>{record.vitalSigns.bloodPressure}</p>
+                                <p className='flex gap-2'><h3 className='font-semibold'>Frequência Cardíaca:</h3> {record.vitalSigns.heartRate}</p>
                         </div>
 
-                        <div>
-                            <h3 className="font-semibold">Inspeção Física:</h3>
-                            <p>Mobilidade Independente: {record.physicalInspection.independentMobility ? 'Sim' : 'Não'}</p>
+                        <div className="bg-gray-100 shadow-lg rounded-lg p-6">
+                                <h3 className="text-2xl block text-gray-700 font-medium pb-6">Inspeção Física:</h3>
+                                <p className='flex gap-2'><h3 className='font-semibold'>Mobilidade Independente:</h3> {record.physicalInspection.independentMobility ? 'Sim' : 'Não'}</p>
                         </div>
 
-                        <div>
-                            <h3 className="font-semibold">Avaliação Sensória:</h3>
-                            <p>Superficial: {record.sensoryAssessment.superficial}</p>
-                            <p>Profunda: {record.sensoryAssessment.deep}</p>
+                            <div className="flex flex-col gap-4 bg-gray-100 shadow-lg rounded-lg p-6">
+                                <h3 className="text-2xl block text-gray-700 font-medium pb-6">Avaliação Sensória:</h3>
+                                <p className='flex gap-2'><h3 className='font-semibold'>Superficial:</h3> {record.sensoryAssessment.superficial}</p>
+                                <p className='flex gap-2'><h3 className='font-semibold'>Profunda:</h3> {record.sensoryAssessment.deep}</p>
                         </div>
 
-                        <div>
-                            <h3 className="font-semibold">Mobilidade do Paciente:</h3>
-                            <p>Tempo de Caminhada de 3 Metros: {record.patientMobility.threeMeterWalkTimeInSeconds} s</p>
+                            <div className="flex flex-col gap-4 bg-gray-100 shadow-lg rounded-lg p-6">
+                                <h3 className="text-2xl block text-gray-700 font-medium pb-6">Mobilidade do Paciente:</h3>
+                                <p className='flex gap-2'><h3 className='font-semibold'>Tempo de Caminhada de 3 Metros:</h3> {record.patientMobility.threeMeterWalkTimeInSeconds} s</p>
                         </div>
 
-                        <div>
-                            <h3 className="font-semibold">Avaliação Fisioterapêutica:</h3>
-                            <p>Diagnóstico: {record.physiotherapyAssessment.diagnosis}</p>
-                            <p>Objetivos do Tratamento: {record.physiotherapyAssessment.treatmentGoals}</p>
+                        <div className="flex flex-col gap-4 bg-gray-100 shadow-lg rounded-lg p-6">
+                                <h3 className="text-2xl block text-gray-700 font-medium pb-6">Avaliação Fisioterapêutica:</h3>
+                                <p className='flex gap-2'><h3 className='font-semibold'>Diagnóstico:</h3> {record.physiotherapyAssessment.diagnosis}</p>
+                                <p className='flex gap-2'><h3 className='font-semibold'>Objetivos do Tratamento: </h3>{record.physiotherapyAssessment.treatmentGoals}</p>
+                            </div>
+                            <div className="flex w-full items-baseline justify-end gap-4 mt-4 pr-8">
+                                <ButtonOne
+                                    texto='Voltar'
+                                    onClick={() => router.back()} />
+
                         </div>
                     </div>
                 ) : (
                     <div>Registro não encontrado.</div>
                 )}
-                <Footer/>
+                <Footer />
             </div>
         </PrivateRoute>
     );

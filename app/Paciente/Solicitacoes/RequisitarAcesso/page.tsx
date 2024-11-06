08d8643d5afd59cfdcf791d34aed810f09e261e6
Felipe Marques <felipe.comarques1208@gmail.com>
2024-11-06 01:08:18 -0300

    feat: add record requests to patient

---
+++ app/Paciente/Solicitacoes/RequisitarAcesso/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import api from "@/app/services/api";
+import NavbarDoctor from "@/app/components/NavbarDoctor";
+import Footer from "@/app/components/Footer";
+import { useRouter } from "next/navigation";
+import ButtonOne from "@/app/components/ButtonOne";
+import PrivateRoute from "@/app/components/PrivateRoute";
+import FooterBar from "@/app/components/Footer";
+
+export default function RequisitarAcesso() {
+  const router = useRouter();
+  const [cpf, setCpf] = useState("");
+  const [recordType, setRecordType] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      if (!recordType) {
+        alert("Por favor, selecione o tipo de ficha avaliativa.");
+        return;
+      }
+
+      const token = localStorage.getItem("access_token");
+      
+      if (!token) {
+        alert("Token de autenticação não encontrado. Faça login novamente.");
+        return;
+      }
+  
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+  
+      // Tenta buscar o paciente pelo CPF
+      const patientResponse = await api.get(`/patients/by-cpf/${cpf}`, config);
+      if (!patientResponse.data || !patientResponse.data.patient) {
+        alert("Paciente não encontrado.");
+        return;
+      }
+  
+      const patientId = patientResponse.data.patient.id;
+  
+      // Tenta solicitar o acesso com base no ID do paciente e no tipo de ficha
+      const accessResponse = await api.patch(
+        `/manage-access/request-access-by-patient-id/${patientId}?recordType=${recordType}`,
+        {},  // Corpo da requisição PATCH (aqui está vazio)
+        config
+      );
+  
+      if (accessResponse.status === 200) {
+        alert("Solicitação de acesso realizada com sucesso.");
+        router.push("/Paciente/Solicitacoes");
+      } else {
+        alert("Erro ao solicitar acesso. Tente novamente.");
+      }
+    } catch (error: any) {
+      if (error.response) {
+        if (error.response.status === 401) {
+          alert("Acesso não autorizado. Verifique seu login e tente novamente.");
+        } else if (error.response.status === 404) {
+          if (error.response.config.url.includes("/patients/by-cpf")) {
+            alert("Paciente não encontrado. Verifique o CPF e tente novamente.");
+          } else if (error.response.config.url.includes("/manage-access/request-access-by-patient-id")) {
+            alert("Registro médico não encontrado para o tipo de ficha selecionado.");
+          }
+        } else if (error.response.status === 409) {
+          alert("Solicitação de acesso já foi realizada anteriormente.");
+        } else {
+          alert("Erro ao processar a solicitação. Tente novamente.");
+        }
+      } else {
+        console.error("Erro ao processar a solicitação:", error);
+        alert("Ocorreu um erro ao buscar o paciente ou solicitar acesso.");
+      }
+    }
+  };
+
+  return (
+    <PrivateRoute requiredUserType="patient">
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <NavbarDoctor />
+        <h2 className="text-4xl font-bold text-gray-700 mb-4 mt-36">Requisitar Acesso</h2>
+        <div>
+          <label className="block mb-2">
+            CPF do Paciente:
+            <input
+              type="text"
+              value={cpf}
+              onChange={(e) => setCpf(e.target.value)}
+              placeholder="Digite o CPF do paciente"
+              className="border rounded w-full py-2 px-3 mt-1"
+            />
+            Tipo de ficha avaliativa:
+            <select
+              value={recordType}
+              onChange={(e) => setRecordType(e.target.value)}
+              className="w-full px-3 py-2 border rounded-md"
+              required
+            >
+              <option value="" disabled>
+                Selecione
+              </option>
+              <option value="Cardio">Cardiorespiratorio</option>
+              <option value="Neurofunctional">Neurofuncional</option>
+              <option value="Trauma">Traumato-ortopédico</option>
+            </select>
+          </label>
+          <ButtonOne texto={"Solicitar"} onClick={handleSubmit} />
+        </div>
+        <FooterBar />
+      </div>
+    </PrivateRoute>
+  );
+}
--- app/Paciente/Solicitacoes/page.tsx
+++ app/Paciente/Solicitacoes/page.tsx
@@ -1,15 +1,231 @@
+"use client";
 import FooterBar from "@/app/components/Footer";
 import NavbarPaciente from "@/app/components/NavbarPaciente";
 import PrivateRoute from "@/app/components/PrivateRoute";
+import { useState, useEffect } from "react";
+import api from "@/app/services/api";
+import ButtonOne from "@/app/components/ButtonOne";
+import { useRouter } from "next/navigation";
 
 export default function Solicitacoes() {
+  const router = useRouter();
+  const [recordsShared, setRecordsShared] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [pendingAuthorization, setPendingAuthorization] = useState<any[]>([]);
+
+  const handleClickView = async (recordId: string, recordType: string) => {
+    if (!recordId) {
+      console.error("Erro: recordId está indefinido");
+      alert("Erro interno: registro não encontrado.");
+      return;
+    }
+
+    const recordRoutes: { [key: string]: string } = {
+      Neuro: "/Medico/Fichas/FichaNeuro",
+      Cardio: "/Medico/Fichas/FichaCardio",
+      Trauma: "/Medico/Fichas/FichaTrauma",
+    };
+
+    const route = recordRoutes[recordType];
+
+    if (!route) {
+      console.log("Tipo de ficha não corresponde.");
+      alert(
+        "Este registro não corresponde a nenhum tipo de ficha que você pode visualizar."
+      );
+      return;
+    }
+
+    localStorage.setItem("currentRecordId", recordId);
+    router.push(route); // Redireciona para a página do registro com base no tipo
+  };
+
+  // Função para autorizar o acesso
+  const handleAuthorize = async (userId: string, recordType: string) => {
+    try {
+      const token = localStorage.getItem("access_token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await api.patch(
+        `/manage-access/pending-authorization/authorize-access/${userId}?recordType=${recordType}`,
+        {},
+        config
+      );
+      alert("Acesso autorizado com sucesso.");
+      setPendingAuthorization(
+        pendingAuthorization.filter((user) => user.userId !== userId)
+      ); // Remove da lista
+    } catch (error) {
+      console.error("Erro ao autorizar acesso:", error);
+      alert("Erro ao autorizar o acesso. Tente novamente.");
+    }
+  };
+
+  // Função para negar o acesso
+  const handleDeny = async (userId: string, recordType: string) => {
+    try {
+      const token = localStorage.getItem("access_token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await api.delete(
+        `/manage-access/pending-authorization/deny-access/${userId}?recordType=${recordType}`,
+        config
+      );
+      alert("Acesso negado com sucesso.");
+      setPendingAuthorization(
+        pendingAuthorization.filter((user) => user.userId !== userId)
+      ); // Remove da lista
+    } catch (error) {
+      console.error("Erro ao negar acesso:", error);
+      alert("Erro ao negar o acesso. Tente novamente.");
+    }
+  };
+
+  // Busca os registros compartilhados e usuários pendentes
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const token = localStorage.getItem("access_token");
+
+        // Buscar registros compartilhados com o usuário
+        const recordsResponse = await api.get(
+          `/manage-access/get-records-shared-with-me`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setRecordsShared(recordsResponse.data);
+
+        // Buscar usuários pendentes de autorização
+        const pendingResponse = await api.get(
+          `/manage-access/pending-authorization`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setPendingAuthorization(pendingResponse.data.pendingAuthorizationUsers);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const recordTypeFullName: { [key: string]: string } = {
+    Neuro: "Neurofuncional",
+    Cardio: "Cardiorespiratório",
+    Trauma: "Traumato-ortopédico",
+  };
+
   return (
     <PrivateRoute requiredUserType="patient">
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <NavbarPaciente />
-        Aqui é as solicitações
+        <div className="flex row-start-2 items-center sm:items-start z-0">
+          <div className="flex flex-col p-4 gap-8 justify-between">
+            <ButtonOne
+              texto="Requisitar acesso"
+              onClick={() =>
+                router.push("/Paciente/Solicitacoes/RequisitarAcesso")
+              }
+            />
+            <div className="flex justify-between">
+              <h2 className="text-3xl font-bold pr-4">
+                Solicitações de acesso e fichas compartilhadas
+              </h2>
+            </div>
+
+            {isLoading ? (
+              <div>Carregando...</div>
+            ) : (
+              <>
+                {pendingAuthorization && pendingAuthorization.length > 0 && (
+                  <div className="flex flex-col gap-8 mt-8">
+                    <h2 className="text-xl font-bold">
+                      Usuários Pendentes de Autorização
+                    </h2>
+                    {pendingAuthorization.map((user: any) => (
+                      <div
+                        key={user.userId}
+                        className="border-b border-gray-300 pb-4 flex justify-between items-center"
+                      >
+                        <div>
+                          <p>
+                            <strong>Nome:</strong> {user.name} {user.surname}
+                          </p>
+                          <p>
+                            <strong>Tipo de Prontuário:</strong>{" "}
+                            {user.recordType}
+                          </p>
+                        </div>
+                        <div className="flex gap-4">
+                          <ButtonOne
+                            texto="Autorizar"
+                            onClick={() =>
+                              handleAuthorize(user.userId, user.recordType)
+                            }
+                          />
+                          <ButtonOne
+                            texto="Negar"
+                            onClick={() =>
+                              handleDeny(user.userId, user.recordType)
+                            }
+                          />
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                )}
+                {recordsShared && recordsShared.length > 0 ? (
+                  <div className="flex flex-col gap-8">
+                    <h2 className="text-xl font-bold">Fichas avaliativas</h2>
+                    {recordsShared.map((record: any) => (
+                      <div
+                        key={record.recordId}
+                        className="border-b border-gray-300 pb-4 flex justify-between items-center"
+                      >
+                        <div>
+                          <p>
+                            <strong>Nome:</strong> {record.name}{" "}
+                            {record.surname}
+                          </p>
+                          <p>
+                            <strong>Tipo de Prontuário:</strong>{" "}
+                            {recordTypeFullName[record.recordType] ||
+                              "Desconhecido"}
+                          </p>
+                        </div>
+                        <ButtonOne
+                          texto="Visualizar"
+                          onClick={() =>
+                            handleClickView(record.recordId, record.recordType)
+                          }
+                        />
+                      </div>
+                    ))}
+                  </div>
+                ) : (
+                  <p>Nenhum prontuário ainda foi compartilhado com você.</p>
+                )}
+              </>
+            )}
+          </div>
+        </div>
         <FooterBar />
       </div>
     </PrivateRoute>
-  )
+  );
 }
